d78d87ece486b21042cca3ff52f11141f89877b4
Aryan Rastogi <rudranshrud@gmail.com>
2025-06-07 22:11:20 +0530

    Initial commit

---
+++ src/main/java/com/example/Zync/ZyncApplication.java
@@ -0,0 +1,13 @@
+package com.example.Zync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Zync/ZyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Zync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
